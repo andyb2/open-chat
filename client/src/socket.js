09b628b92a19ab836099291cf45f0c8f3c728dc5
Andrew Boyle <andrewboyle@ab.local>
2022-08-01 16:24:32 -0400

    when the client disconnects a series of socket offs will remove the event listeners. When connecting to the app sometimes the listeners would be duplicated causing them to fire more than once

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,18 +1,18 @@
 import io from 'socket.io-client';
 import { store } from './app/store';
-import { userList, removeUser, messages, createPrivateRoom } from './app/reducer/roomSlice';
+import { userList, removeUser, messages, createPrivateRoom, resetRoomState } from './app/reducer/roomSlice';
+import { resetUserState, user } from './app/reducer/userSlice';
 
 const socket = io(window.location.origin);
 
 socket.on('connect', () => {
-
+    
     socket.on('join-room', (usersInRoom) => {
         store.dispatch(userList(usersInRoom));
     });
 
     socket.on('message', ({ message, user, timeOfMessage, privateMessage, sender }) => {
         if (privateMessage) {
-            console.log(`RECEIVED`, user, sender, message, privateMessage)
             store.dispatch(createPrivateRoom({ socketId: sender, username: user.username }));
         }
         store.dispatch(messages({ message, user, timeOfMessage, privateMessage }));
@@ -21,6 +21,16 @@ socket.on('connect', () => {
     socket.on('remove-room-user', (roomData) => {
         store.dispatch(removeUser(roomData));
     });
+
+    socket.on('disconnect', () => {
+        store.dispatch(resetUserState());
+        store.dispatch(resetRoomState());
+        socket.off('join-room');
+        socket.off('message');
+        socket.off('remove-room-user');
+        socket.off('disconnect');
+    })
+    
 });
 
 export default socket;
